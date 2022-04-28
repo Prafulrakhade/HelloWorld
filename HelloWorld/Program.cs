c4cb6c0b59f6af3f7fdcfabb381a8b1ac6886139
Praful <prafulrakhade02@gmail.com>
2022-04-28 11:34:43 +0530

    [praful] added : program

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Program p = new Program();
+            p.PrintName();
+        }
+        /// <summary>
+        /// it is a method 
+        /// </summary>
+        public void PrintName()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
